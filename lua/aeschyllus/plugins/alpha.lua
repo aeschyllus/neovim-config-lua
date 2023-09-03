393928682788986891d4184adb3665dc283809cb
aeschyllus <lapadamarkanthony@gmail.com>
2023-09-03 19:43:46 +0800

    custom alpha greet

--- lua/aeschyllus/plugins/alpha.lua
+++ lua/aeschyllus/plugins/alpha.lua
@@ -1,3 +1,22 @@
+math.randomseed(os.time())
+
+local quotes = {
+	-- Marcus Aurelius
+	"               The best way to anger is silence.",
+	"        The best revenge is not to be like your enemy.",
+	"               Confine yourself to the present.",
+	"          You can commit injustice by doing nothing.",
+	"               Each day provides its own gifts.",
+	"          Give yourself a gift, the present moment.",
+
+	-- Seneca
+	"     We suffer more often in imagination than in reality.",
+	"            While we wait for life, life passes.",
+	"           Life is long, if you know how to use it.",
+	"                      Hurry up and live.",
+	-- "▓▓▓▓▓▒▒▓███▓▒▓▓▓█▓██▒▒▒▓▓▓▒▒▓▓▓▓▓▓▒▒▒█▒░▒▒▒▓▓███▒▒▒▓▓▓▒░░▒██▓▓",
+}
+
 return {
 	"goolord/alpha-nvim",
 	config = function()
@@ -36,6 +55,7 @@ return {
 			[[ ▓▓▓▒▓██▓▓▒▒▓▓▒█▓▓█▓▒▒▓▓▓▒▓▓▓███▓▒▒▒▒▓▒░▒▒▓████▓▒▒▓▓▓▒▒▒▓▓▓▓  ]],
 			[[▓▓▓▓▓▒▒▓███▓▒▓▓▓█▓██▒▒▒▓▓▓▒▒▓▓▓▓▓▓▒▒▒█▒░▒▒▒▓▓███▒▒▒▓▓▓▒░░▒██▓▓]],
 			[[                                                              ]],
+			quotes[math.random(#quotes)],
 		}
 
 		dashboard.section.buttons.val = {
