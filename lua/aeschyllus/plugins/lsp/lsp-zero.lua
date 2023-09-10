f70284022860b9daa90aed3e729bded907c5f96b
aeschyllus <lapadamarkanthony@gmail.com>
2023-09-11 04:38:11 +0800

    volar takeover mode

--- lua/aeschyllus/plugins/lsp/lsp-zero.lua
+++ lua/aeschyllus/plugins/lsp/lsp-zero.lua
@@ -1,26 +1,35 @@
 return {
-  'VonHeikemen/lsp-zero.nvim',
-  branch = 'v2.x',
-  dependencies = {
-    -- LSP Support
-    {'neovim/nvim-lspconfig'},             -- Required
-    {'williamboman/mason.nvim'},           -- Optional
-    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+	"VonHeikemen/lsp-zero.nvim",
+	branch = "v2.x",
+	dependencies = {
+		-- LSP Support
+		{ "neovim/nvim-lspconfig" }, -- Required
+		{ "williamboman/mason.nvim" }, -- Optional
+		{ "williamboman/mason-lspconfig.nvim" }, -- Optional
 
-    -- Autocompletion
-    {'hrsh7th/nvim-cmp'},     -- Required
-    {'hrsh7th/cmp-nvim-lsp'}, -- Required
-    {'L3MON4D3/LuaSnip'},     -- Required
-  },
-  config = function()
-    local lsp = require("lsp-zero").preset({})
+		-- Autocompletion
+		{ "hrsh7th/nvim-cmp" }, -- Required
+		{ "hrsh7th/cmp-nvim-lsp" }, -- Required
+		{ "L3MON4D3/LuaSnip" }, -- Required
+	},
+	config = function()
+		local lsp = require("lsp-zero").preset({})
 
-    lsp.on_attach(function(client, bufnr)
-      -- see :help lsp-zero-keybindings
-      -- to learn the available actions
-      lsp.default_keymaps({ buffer = bufnr })
-    end)
+		lsp.on_attach(function(_, bufnr)
+			-- see :help lsp-zero-keybindings
+			-- to learn the available actions
+			lsp.default_keymaps({ buffer = bufnr })
+		end)
 
-    lsp.setup()
-  end,
+		lsp.ensure_installed({
+			"lua_ls",
+			"volar",
+		})
+
+		require("lspconfig").volar.setup({
+			filetypes = { "vue", "typescript", "javascript" },
+		})
+
+		lsp.setup()
+	end,
 }
