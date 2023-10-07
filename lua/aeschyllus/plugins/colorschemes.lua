1f5052486cc6f1f0d0dffdc45e2d31dd132def3a
aeschyllus <lapadamarkanthony@gmail.com>
2023-10-08 07:18:46 +0800

    Added Tokyo Night colorscheme

--- lua/aeschyllus/plugins/colorschemes.lua
+++ lua/aeschyllus/plugins/colorschemes.lua
@@ -1,15 +1,24 @@
 return {
-  {
-    "Everblush/nvim",
-    name = "everblush",
-    lazy = true,
-  },
-  {
-    "neanias/everforest-nvim",
-    version = false,
-    priority = 1000, -- load first before all other plugins
-    config = function()
-      vim.cmd("colorscheme everforest")
-    end,
-  },
+	{
+		"Everblush/nvim",
+		name = "everblush",
+		lazy = true,
+	},
+	{
+		"neanias/everforest-nvim",
+		lazy = true,
+		-- version = false,
+		-- priority = 1000, -- load first before all other plugins
+		-- config = function()
+		-- 	vim.cmd("colorscheme everforest")
+		-- end,
+	},
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd("colorscheme tokyonight")
+		end,
+	},
 }
