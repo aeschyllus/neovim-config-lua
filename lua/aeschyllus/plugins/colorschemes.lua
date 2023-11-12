c3dd5ace32c915b690b479520220e3c323fe1622
aeschyllus <lapadamarkanthony@gmail.com>
2023-11-12 20:16:54 +0800

    removed unused colorschemes

--- lua/aeschyllus/plugins/colorschemes.lua
+++ lua/aeschyllus/plugins/colorschemes.lua
@@ -1,24 +1,10 @@
 return {
 	{
-		"Everblush/nvim",
-		name = "everblush",
-		lazy = true,
-	},
-	{
 		"neanias/everforest-nvim",
-		lazy = true,
-		-- version = false,
-		-- priority = 1000, -- load first before all other plugins
-		-- config = function()
-		-- 	vim.cmd("colorscheme everforest")
-		-- end,
-	},
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
+		version = false,
+		priority = 1000, -- load first before all other plugins
 		config = function()
-			vim.cmd("colorscheme tokyonight")
+			vim.cmd("colorscheme everforest")
 		end,
 	},
 }
