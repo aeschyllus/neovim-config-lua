d84ee89dba7b5b7693cad775bd5d6676ba0134eb
aeschyllus <lapadamarkanthony@gmail.com>
2024-05-25 10:08:43 +0800

    change default theme

--- lua/aeschyllus/plugins/colorschemes.lua
+++ lua/aeschyllus/plugins/colorschemes.lua
@@ -1,27 +1,10 @@
 return {
-	-- {
-	-- 	"neanias/everforest-nvim",
-	-- 	version = false,
-	-- 	priority = 1000, -- load first before all other plugins
-	-- 	config = function()
-	-- 		vim.cmd("colorscheme everforest")
-	-- 	end,
-	-- },
 	{
-    "nobbmaestro/nvim-andromeda",
-    version = false,
-    priority = 1000, -- load first before all other plugins
-    dependencies = {
-      { "tjdevries/colorbuddy.nvim" },
-    },
-    config = function()
-      require("andromeda").setup()
-    end,
+		"neanias/everforest-nvim",
+		version = false,
+		priority = 1000, -- load first before all other plugins
+		config = function()
+			vim.cmd("colorscheme everforest")
+		end,
 	},
-  {
-    "norcalli/nvim-colorizer.lua",
-    config = function()
-      require("colorizer").setup()
-    end,
-  }
 }
